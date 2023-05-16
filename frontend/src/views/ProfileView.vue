e3a253c4c190164dd0eae2aa2778682c5e3a1a61
Sayan Ghosh <sgic2001@gmail.com>
2023-05-16 11:15:30 +0530

    try to fix router issue on page reload and sse wrong url

--- frontend/src/views/ProfileView.vue
+++ frontend/src/views/ProfileView.vue
@@ -1,81 +1,84 @@
 <script setup lang="ts">
-import type { UserType } from '@/types/UserType';
-import { onMounted, ref } from 'vue';
-import { apiClient } from '@/axios';
-import { RouterLink, useRouter } from 'vue-router';
-import ProfileCard from '@/components/ProfileCard.vue';
-import UserPosts from '@/components/UserPosts.vue';
-import ProfileStats from '@/components/ProfileStats.vue';
-import { PHOTO_URL_PREFIX } from '@/defaults';
+import type { UserType } from '@/types/UserType'
+import { onMounted, ref } from 'vue'
+import { apiClient } from '@/axios'
+import { RouterLink, useRouter } from 'vue-router'
+import ProfileCard from '@/components/ProfileCard.vue'
+import UserPosts from '@/components/UserPosts.vue'
+import ProfileStats from '@/components/ProfileStats.vue'
+import { PHOTO_URL_PREFIX } from '@/defaults'
 
-const router = useRouter();
+const router = useRouter()
 
-const user = ref({} as UserType);
+const user = ref({} as UserType)
 
 const getProfile = () => {
-    apiClient.get('/user')
-    .then(res => {
-        user.value = res.data;
+  apiClient
+    .get('/user')
+    .then((res) => {
+      user.value = res.data
     })
-    .catch(err => {
-        if (err.response.status == 401) {
-            alert('You are not authorized to access this page. Please login again.');
-            router.push('/login');
-        }
-    });
+    .catch((err) => {
+      if (err.response.status == 401) {
+        alert('You are not authorized to access this page. Please login again.')
+        router.push('/login')
+      }
+    })
 }
 
-const exportPosts = () =>{
-    // send a get request to /export which starts backend job and start listening to sse stream for job status
-    apiClient.get('/export')
+const exportPosts = () => {
+  // send a get request to /export which starts backend job and start listening to sse stream for job status
+  apiClient
+    .get('/export')
     .then(() => {
-        // create a new event source
-        const eventSource = new EventSource(PHOTO_URL_PREFIX + '/stream');
-        // listen to the event source for the job status
-        eventSource.addEventListener('export_posts'+user.value.id, event => {
-            const path = JSON.parse(event.data).path;
-            const abspath = PHOTO_URL_PREFIX + path;
-            // download the file
-            window.open(abspath, '_blank');
-            eventSource.close();
-        })
-    }).catch(err => {
-        console.log(err);
-    });
+      // create a new event source
+      const eventSource = new EventSource(PHOTO_URL_PREFIX + 'stream')
+      // listen to the event source for the job status
+      eventSource.addEventListener('export_posts' + user.value.id, (event) => {
+        const path = JSON.parse(event.data).path
+        const abspath = PHOTO_URL_PREFIX + path
+        // download the file
+        window.open(abspath, '_blank')
+        eventSource.close()
+      })
+    })
+    .catch((err) => {
+      console.log(err)
+    })
 }
 
 onMounted(() => {
-    getProfile();
-});
+  getProfile()
+})
 </script>
 
 <template>
   <div class="profile-view">
     <ProfileCard :user="user" />
     <div class="center">
-        <RouterLink :to="{name: 'edit-profile'}" class="btn btn-primary">
-            <i class="fas fa-pencil-alt    "></i>
-            Edit Profile
-        </RouterLink>
+      <RouterLink :to="{ name: 'edit-profile' }" class="btn btn-primary">
+        <i class="fas fa-pencil-alt"></i>
+        Edit Profile
+      </RouterLink>
     </div>
-    <hr>
+    <hr />
     <ProfileStats :user="user" />
     <div class="d-flex align-items-center justify-content-between">
-        <h2>Posts: </h2>
-        <button class="btn btn-outline-primary" @click="exportPosts">
-            <i class="fas fa-file-export"></i>
-            Export Posts
-        </button>
+      <h2>Posts:</h2>
+      <button class="btn btn-outline-primary" @click="exportPosts">
+        <i class="fas fa-file-export"></i>
+        Export Posts
+      </button>
     </div>
     <UserPosts :user="user" />
-    <hr>
+    <hr />
   </div>
 </template>
 
 <style scoped lang="scss">
-.center{
-    display: flex;
-    justify-content: center;
-    align-items: center;
+.center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
