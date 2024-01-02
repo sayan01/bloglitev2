5316934dac77695cfaced9f21e46e9b7744c8537
Sayan Ghosh <sgic2001@gmail.com>
2024-01-02 13:16:33 +0530

    fix vulnerability in axios and werkzeug

--- frontend/src/axios.ts
+++ frontend/src/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'https://bloglite.sayn.work',
+  baseURL: 'http://127.0.0.1:5000',
   headers: {
     'Content-Type': 'application/json'
   }
